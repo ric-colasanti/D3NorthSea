d623188f30e2acc125887168336300a60efb2d23
ric-colasanti <59831865+ric-colasanti@users.noreply.github.com>
2021-10-04 14:08:23 +0100

    Add interactive on click change color

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ var mainfun = function(){
 }
 
 var d3refresh = function(){
-    var svg = d3.select("#chart").attr("width",500);
-
+    var svg = d3.select("#chart");
+    width = parseInt(svg.style("width"));
+    height = parseInt(svg.style("height"))
+    console.log(width,height)
     var circle = svg.selectAll("circle")
         .data([32, 57, 112, 293,90]);
     var circleEnter = circle.enter()
@@ -16,5 +18,9 @@ var d3refresh = function(){
     .attr("cy", 60)
     .attr("fill","orange")
     .attr("cx", (d, i) =>i * 50 + 35) // note the varable name d ca be replaced with any vriable name
-    .attr("r", d => Math.sqrt(d));
+    .attr("r", d => Math.sqrt(d))
+    .on("click", function(){
+        console.log("clicked")
+        d3.select(this).attr("fill","pink")
+    });
 }

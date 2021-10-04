4ca8704856620cec7df5ffb11148916d35b7a9a3
ric-colasanti <59831865+ric-colasanti@users.noreply.github.com>
2021-10-04 15:10:41 +0100

    Add and remove data from d3

--- js/main.js
+++ js/main.js
@@ -1,26 +1,43 @@
-var testfun = function(){
+var testfun = function () {
     console.log("test")
 }
 
-var mainfun = function(){
+var mainfun = function () {
     console.log("main")
 }
 
-var d3refresh = function(){
+var d3refresh = function () {
+    var testData = [32, 57, 112, 293,90]
+    document.getElementById("button")
     var svg = d3.select("#chart");
     width = parseInt(svg.style("width"));
     height = parseInt(svg.style("height"))
-    console.log(width,height)
-    var circle = svg.selectAll("circle")
-        .data([32, 57, 112, 293,90]);
-    var circleEnter = circle.enter()
-    .append("circle")
-    .attr("cy", 60)
-    .attr("fill","orange")
-    .attr("cx", (d, i) =>i * 50 + 35) // note the varable name d ca be replaced with any vriable name
-    .attr("r", d => Math.sqrt(d))
-    .on("click", function(){
-        console.log("clicked")
-        d3.select(this).attr("fill","pink")
-    });
+    var update = function (svg, gData) {
+        console.log("here",gData)
+        var circle = svg.selectAll("circle")
+            .data(gData);
+        circle.enter()
+            .append("circle")
+            .attr("cy", 60)
+            .attr("fill", "orange")
+            .attr("cx", (d, i) => i * 50 + 35) // note the varable name d ca be replaced with any vriable name
+            .attr("r", d => Math.sqrt(d))
+            .on("click", function () {
+                console.log("clicked")
+                d3.select(this).attr("fill", "pink")
+            });
+        circle.exit().remove()
+       
+    }
+    update(svg,testData )
+    document.getElementById("btnRemove").onclick = function(){
+        testData.pop()
+        update(svg,testData)
+    }
+    document.getElementById("btnAdd").onclick = function(){
+        randomNum = Math.round(Math.random() * 501);  // 0 to 100
+        console.log(randomNum);
+        testData.push(randomNum)
+        update(svg,testData)
+    }
 }

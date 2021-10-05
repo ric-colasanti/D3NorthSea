752406bbe61e570012f06d49cbab92d4260c9f07
Dr Ric Colasanti <developer.colasanti@gmail.com>
2021-10-05 19:37:09 +0100

    adde update and trasition to d3 used join

--- js/main.js
+++ js/main.js
@@ -12,22 +12,38 @@ var d3refresh = function () {
     var svg = d3.select("#chart");
     width = parseInt(svg.style("width"));
     height = parseInt(svg.style("height"))
+
+
+
     var update = function (svg, gData) {
-        console.log("here",gData)
+        
+        console.log("here",testData)
         var circle = svg.selectAll("circle")
-            .data(gData);
-        circle.enter()
-            .append("circle")
-            .attr("cy", 60)
-            .attr("fill", "orange")
+            .data(testData);
+        circle.join(
+            enter =>
+            enter.append("circle")
+                .attr("cy", 60)
+                .attr("fill", "orange")
+                .attr("cx", (d, i) => i * 50 + 35) // note the varable name d ca be replaced with any vriable name
+                .attr("r", d => Math.sqrt(d))
+                .on("click", function () {
+                    console.log("clicked")
+                    d3.select(this).attr("fill", "green")
+                }),
+            update =>
+            update
+            .transition()
+            .duration(750)
             .attr("cx", (d, i) => i * 50 + 35) // note the varable name d ca be replaced with any vriable name
-            .attr("r", d => Math.sqrt(d))
-            .on("click", function () {
-                console.log("clicked")
-                d3.select(this).attr("fill", "pink")
-            });
-        circle.exit().remove()
-       
+            .attr("r", d => Math.sqrt(d)),
+            exit =>
+            exit
+            .transition()
+            .duration(750)
+            .attr("r",0)
+            .remove()
+        );
     }
     update(svg,testData )
     document.getElementById("btnRemove").onclick = function(){
@@ -44,6 +60,6 @@ var d3refresh = function () {
         randomNum = Math.round(Math.random() * 501);  // 0 to 100
         console.log(randomNum);
         testData[1] = randomNum
-        update(svg,testData)
+        update(svg,[300,400])
     }
 }

741c6785288b8aa30340680697bb47f4f7396ddc
ric-colasanti <developer.colasanti@gmail.com>
2021-10-08 11:50:41 +0000

    Added D3Joy  secton showing entry update exit with text in list

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ var mainfun = function () {
 
 var d3refresh = function () {
     var testData = [32, 57, 112, 293, 90]
-    document.getElementById("button")
     var svg = d3.select("#chart");
     width = parseInt(svg.style("width"));
     height = parseInt(svg.style("height"))
@@ -18,8 +17,7 @@ var d3refresh = function () {
     var update = function (svg, gData) {
 
         console.log("here", testData)
-        var circle = svg.selectAll("circle")
-            .data(testData);
+        var circle = svg.selectAll("circle").data(testData);
         circle.join(
             enter =>
             enter.append("circle")
@@ -62,4 +60,62 @@ var d3refresh = function () {
         testData[1] = randomNum
         update(svg, [300, 400])
     }
+}
+
+var d3joy = function(){
+    var data =[[0,"First"],[1,"Second"],[2,"Third"],[3,"Fourth"],[4,"Fifth"],[5,"Sixth"]];
+    console.log("d3joy")
+    counter = 6
+    var update = function(){
+        var d3Select = d3.select("#list");
+        console.log("update ",data)
+        var list = d3Select.selectAll("li").data(data,d=>d[0]);
+        list.join(
+            enter => 
+            enter.append("li")
+            .text(d=>d[1])
+            .on("click", function(){
+                this.toggle = !this.toggle; // declared variable setting it to true
+                d3.select(this)
+                .transition().duration(1000)
+                .style("color", this.toggle?"purple":"orange");
+            })
+            .style("color","rgba(255,255,255, 0.0)")
+            .transition().duration(1000)
+            .style("color","orange"),
+            update =>
+            update.text(d=>d[1]),
+            exit =>
+            exit
+            .transition().duration(1000)
+            .style("color","rgba(255,255,255, 0.0)")
+            .remove()
+        )
+    }
+
+    var changeAllColors = function(){
+        var d3Select = d3.select("#list");
+        console.log("change ",data)
+        var list = d3Select.selectAll("li").data(data,d=>d[0]);
+        list.transition().duration(2000).style("color",function(d){if (d[0]==1){
+            return "blue"}
+            return "rgba(190,190,190, 0.3)";
+        }) 
+    }
+
+    document.getElementById("btnRemove").onclick = function () {
+        data.splice(1,1);
+        update();
+    }
+    document.getElementById("btnChange").onclick = function () {
+        //data[2][1] = "changed";
+        changeAllColors()
+        //update();
+    }
+    document.getElementById("btnAdd").onclick = function () {
+        data.splice(2,0,[counter,"New at third pos"]);
+        counter++;
+        update();
+    }
+    update()
 }

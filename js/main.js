c159919a1b62023f70ee2f8e66db1a8c94949b19
Dr Ric Colasanti <developer.colasanti@gmail.com>
2021-10-25 10:27:20 +0100

    Add network graph

--- js/main.js
+++ js/main.js
@@ -8,9 +8,14 @@ var mainfun = function () {
 
 var d3refresh = function () {
     var testData = []
-    for (let i = 0; i <5; i++) {
-        testData.push({"id":i,"value":Math.floor(Math.random() * 50), "xPos" : Math.floor(Math.random() * 400), "yPos" : Math.floor(Math.random() * 400)});   
-    } 
+    for (let i = 0; i < 5; i++) {
+        testData.push({
+            "id": i,
+            "value": Math.floor(Math.random() * 50),
+            "xPos": Math.floor(Math.random() * 400),
+            "yPos": Math.floor(Math.random() * 400)
+        });
+    }
     console.log(testData)
     var svg = d3.select("#chart");
     width = parseInt(svg.style("width"));
@@ -19,36 +24,36 @@ var d3refresh = function () {
 
 
 
-    var update = function (svg, gData,d=1000) {
+    var update = function (svg, gData, d = 1000) {
         console.log("here", gData)
         const circles = svg.selectAll("circle").data(testData, (d) => d.id);
         circles.exit()
-        .transition()
-        .duration(d)
-        .attr("r", 0)
-        .remove();
+            .transition()
+            .duration(d)
+            .attr("r", 0)
+            .remove();
         circles.enter().append("circle")
             .attr("cy", d => d.xPos)
-            .attr("cx", d=> d.yPos)
+            .attr("cx", d => d.yPos)
             .on("click", function () {
                 console.log("clicked")
                 d3.select(this).attr("fill", "green")
             })
             .attr("fill", "orange")
-            .attr("stroke","black")
+            .attr("stroke", "black")
             .transition().duration(d)
-            .attr("r",d => d.value)
+            .attr("r", d => d.value)
         circles.merge(circles)
-        .transition().duration(d)
-        .attr("cy", d => d.xPos)
-        .attr("cx", d=> d.yPos)
-        .attr("r",d => d.value)
-         // note the varable name d ca be replaced with any vriable name
+            .transition().duration(d)
+            .attr("cy", d => d.xPos)
+            .attr("cx", d => d.yPos)
+            .attr("r", d => d.value)
+        // note the varable name d ca be replaced with any vriable name
 
     }
-    update(svg, testData,0)
+    update(svg, testData, 0)
     document.getElementById("btnRemove").onclick = function () {
-        testData.splice(3,1)
+        testData.splice(3, 1)
         console.log(testData)
         update(svg, testData)
     }
@@ -56,11 +61,16 @@ var d3refresh = function () {
         randomNum = Math.round(Math.random() * 50); // 0 to 100
         console.log(randomNum);
         counter++
-        testData.push({"id":counter,"value":Math.floor(Math.random() * 50), "xPos" : Math.floor(Math.random() * 400), "yPos" : Math.floor(Math.random() * 400)})
+        testData.push({
+            "id": counter,
+            "value": Math.floor(Math.random() * 50),
+            "xPos": Math.floor(Math.random() * 400),
+            "yPos": Math.floor(Math.random() * 400)
+        })
         update(svg, testData)
     }
     document.getElementById("btnChange").onclick = function () {
-        testData.forEach(function(d){
+        testData.forEach(function (d) {
             d.xPos = Math.floor(Math.random() * 400);
             d.yPos = Math.floor(Math.random() * 400);
         })
@@ -106,7 +116,7 @@ var d3joy = function () {
             update.text(d => d[1]),
             exit =>
             exit
-            .style("color","blue")
+            .style("color", "blue")
             .transition().duration(1000)
             .style("color", "rgba(255,255,255, 0.0)")
             .remove()
@@ -146,7 +156,7 @@ var d3joy = function () {
     document.getElementById("btnHighlight").onclick = function () {
         changeAllColors();
     }
-    
+
 
     window.addEventListener("scroll", function () {
         loadBtn = document.getElementById("btnLoad").getBoundingClientRect().top
@@ -193,10 +203,99 @@ var d3joy = function () {
             data.splice(1, 1);
             update();
             removed = true
-        }        
-        if ((hightlightBtn <= xList) && (!hilighted)) {width
+        }
+        if ((hightlightBtn <= xList) && (!hilighted)) {
+            width
             changeAllColors();
             hilighted = true
-        }  
+        }
     });
+}
+
+
+var d3graph = function () {
+
+    // set the dimensions and margins of the graph
+    const margin = {
+            top: 10,
+            right: 30,
+            bottom: 30,
+            left: 40
+        },
+        width = 400 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    const svg = d3.select("#my_dataviz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            `translate(${margin.left}, ${margin.top})`);
+
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json").then(function (data) {
+
+        // Initialize the links
+        const link = svg
+            .selectAll("line")
+            .data(data.links)
+            .join("line")
+            .style("stroke", "white")
+            .attr("x1",width/2)
+            .attr("y1",height/2)
+            .attr("x2", width/2)
+            .attr("y2", height/2)
+
+        // Initialize the nodes
+        const node = svg
+            .selectAll("circle")
+            .data(data.nodes)
+            .join("circle")
+            .attr("r", 0)
+            .style("fill", "blue")
+            .attr("cx",height/2)
+            .attr("cy",width/2)
+
+        // Let's list the force we wanna apply on the network
+        const simulation = d3.forceSimulation(data.nodes) // Force algorithm is applied to data.nodes
+            .force("link", d3.forceLink() // This force provides links between nodes
+                .id(function (d) {
+                    return d.id;
+                }) // This provide  the id of a node
+                .links(data.links) // and this the list of links
+            )
+            .force("charge", d3.forceManyBody().strength(-100)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+            .force("center", d3.forceCenter(width / 2, height / 2)) // This force attracts nodes to the center of the svg area
+            .on("end", ticked);
+
+        // This function is run at each iteration of the force algorithm, updating the nodes position.
+        function ticked() {
+            link
+                .attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                })
+                .style("stroke", "black");
+
+            node
+                .attr("r", 10)
+                .attr("cx", function (d) {
+                    return d.x;
+                })
+                .attr("cy", function (d) {
+                    return d.y;
+                });
+        }
+
+    });
+
 }

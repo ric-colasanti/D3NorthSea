0cf4bdd833f0648e41f21019d4a79a6e4b9bbbec
Dr Ric Colasanti <developer.colasanti@gmail.com>
2021-11-08 15:44:16 +0000

    add zoom and pan

--- js/graph2.js
+++ js/graph2.js
@@ -1,4 +1,4 @@
-var d3graph = function () {
+var d3graph2 = function () {
 
     function randInt(max) {
         return Math.floor((Math.random() * max))
@@ -28,6 +28,19 @@ var d3graph = function () {
         })
     }
     console.log(data);
+
+    let zoom = d3.zoom()
+	.on('zoom', handleZoom);
+
+    function handleZoom(e) {
+        d3.select('svg g')
+            .attr('transform', e.transform);
+    }
+    
+    function initZoom() {
+        d3.select('svg')
+            .call(zoom);
+    }
     // set the dimensions and margins of the graph
     const margin = {
             top: 10,
@@ -44,22 +57,6 @@ var d3graph = function () {
         .append("g")
         .attr("transform",
             `translate(${margin.left}, ${margin.top})`);
-            let zoom = d3
-            .zoom()
-            .scaleExtent([0.05, 20])
-            .translateExtent([
-                [0, 0],
-                [width, height]
-            ])
-            .on("zoom", () => {
-                svg.select("#view").attr("transform", d3.event.transform);
-                xGroup.call(xAxis.scale(d3.event.transform.rescaleX(xScale)));
-                yGroup.call(yAxis.scale(d3.event.transform.rescaleY(yScale)));
-            });
-        svg.call(zoom);
-        
-
-
     function update() {
         // append the svg object to the body of the page
         document.getElementById("btnChange").className = "btn btn-secondary"
@@ -93,9 +90,7 @@ var d3graph = function () {
             return d.linked ? "blue" : "orange"
             }),
             update=>
-            update
-            .transition().duration(1000)
-            .style("fill", function (d) {
+            update.style("fill", function (d) {
                 return d.linked ? "blue" : "orange"
                 })
             .raise(),
@@ -122,7 +117,9 @@ var d3graph = function () {
         
         // This function is run at each iteration of the force algorithm, updating the nodes position.
         function ticked() {
-
+            for (let i = 0; i < 50; i++) {
+                simulation.tick();
+              }
             node
                 .attr("r", d=>d.size)
                 .attr("cx", function (d) {
@@ -161,7 +158,7 @@ var d3graph = function () {
         addData()
         update()
     }
-    
+    initZoom();
     update()
 
 }
